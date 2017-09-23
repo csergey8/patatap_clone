64aaaa37de99a749d8765144457719fdd14c3e75
csergey8 <c.sergey@protonmail.com>
2017-09-23 09:56:38 +0300

    add howler js code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
 var circles = [];
 
-
+var keyData = {
+    a: {
+        color: "purple",
+        sound: new Howl({
+            src: ['sounds/bubbles.mp3']
+        })
+    },
+    s: {
+        color: "green",
+        sound: new Howl({
+            src: ['sounds/clay.mp3']
+        })
+    },
+    d: {
+        color: "red",
+        sound: new Howl({
+            src: ['sounds/confetti.mp3']
+        })
+    }
+}
 
 function onKeyDown(event) {
     var maxPoint = new Point(view.size.width, view.size.height);
     var randomPoint = Point.random();
     var point = maxPoint * randomPoint;
-    var newCircle = new Path.Circle(point, 100);
-    newCircle.fillColor = "purple";
+    var newCircle = new Path.Circle(point, 500);
+    //console.log(keyData[event.key].color);
+    if (keyData[event.key]) {
+        newCircle.fillColor = (keyData[event.key].color);
+    } else {
+        newCircle.fillColor = "white";
+    }
+    //keyData[event.key].sound.play();
 
     circles.push(newCircle);
 };
 
 function onFrame(event) {
     for (var i = 0; i < circles.length; i++) {
-        circles[i].fillColor.hue += 1;
+        circles[i].fillColor.hue += 10;
         circles[i].scale(.86);
     }
 }
